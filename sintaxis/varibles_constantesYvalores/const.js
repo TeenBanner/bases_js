af3767e336b8355e6175182f5044d29670ee856e
poncho <alfonso.dancerod@gmail.com>
2023-11-12 19:51:10 -0600

    datos

---
+++ sintaxis/varibles_constantesYvalores/const.js
@@ -0,0 +1,13 @@
+// constantes
+// las constantes van a ser una variable que va a ser constante es decir no se va a poder modificar su valor a lo largo del tiempo de ejecucion
+const nombre = 'poncho'
+// en una constante alamacenamos datos que no queremos cambiar 
+
+// este nombre no va a cambiar y por buenas practicas debemos nombrarlas en mayusculas y separar las palabras con guiones amenos que deban de ser funciones
+const NOMBRE_EMPRESA = 'EDteam'
+
+/* una constante no puede ser declarada sin asignarle un valor y tampoco podemos asignarle un valor despues de su declaracion
+    const NOMBRE
+    ni esto
+    NOMBRE = valor
+*/
