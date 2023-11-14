c96d55af527eb651acbbf7d3e1b6ce5a62dd6de7
poncho <alfonso.dancerod@gmail.com>
2023-11-14 11:54:41 -0600

    cambios rapidos

---
+++ sintaxis/operadores/operadores_unarios_y_ternarios.js
@@ -0,0 +1,15 @@
+// operadores unarios
+
+let a = 10 
+// aqui el valor se incrementa
+a++
+// aqui disminuye el valor
+a--
+
+
+// operadores ternarios
+
+let a2 = 5
+let b = 8
+
+let c = b > a2 ? "es verdad " : "es falso"
